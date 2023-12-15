c2b99b7519f669b6a77b404ffdcf7b81a8161369
mohamed1408 <mohamedanastsi@gmail.com>
2023-12-15 13:58:27 +0530

    rerun

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -11,16 +11,17 @@ export class AuthGuardService implements CanActivate {
   user: any;
   constructor(public jwtHelper: JwtHelperService, public router: Router) {
     this.token = localStorage.getItem("utoken") || ""
+    this.ctoken = localStorage.getItem("ctoken") || ""
   }
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
     this.user = JSON.parse(localStorage.getItem("user") || '{"role": ""}')
-    console.log(next.routeConfig?.path)
+    console.log(next.routeConfig?.path, this.jwtHelper.getTokenExpirationDate(this.token))
     if ((next.routeConfig?.path != '' && next.routeConfig?.path != 'lock') && !this.jwtHelper.getTokenExpirationDate(this.token)) {
       console.log("Expired!")
       this.router.navigate(['']);
       return false;
-    } else if (!next.data.role.includes(this.user.role.toLowerCase())&&!next.data.role.includes("all")) {
-      console.log(next.url,this.user.role,"No Role")
+    } else if (!next.data.role.includes(this.user.role.toLowerCase()) && !next.data.role.includes("all")) {
+      console.log(next.url, this.user.role, "No Role")
       this.router.navigate(['lock']);
       return false;
     }
