48839402ad24a94413880cfdcdb11c2aac45f369
mohamed1408 <mohamedanastsi@gmail.com>
2024-01-09 17:29:28 +0530

    split payment [ T R A N S A C T I O N   L I S T ]

--- src/app/transaction-list/transaction-list.component.ts
+++ src/app/transaction-list/transaction-list.component.ts
@@ -4,9 +4,10 @@ import { Ng2SearchPipe } from 'ng2-search-filter';
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Observable, OperatorFunction } from 'rxjs';
 import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 import { daterangepicker } from '../../assets/dist/js/datePickerHelper';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Transaction } from '../model/model';
 
 declare function setHeightWidth(): any;
 
@@ -18,6 +19,7 @@ declare function setHeightWidth(): any;
 })
 export class TransactionListComponent implements OnInit {
   @ViewChild('selection', { static: false }) private selection: ElementRef | any;
+  @ViewChild('transactioneditmdl', { static: false }) private transactioneditmdl: ElementRef | any;
   @ViewChild('compare_modal', { static: true }) private compare_modal!: ElementRef;
 
   searchTerm: string = ""
@@ -27,7 +29,42 @@ export class TransactionListComponent implements OnInit {
   fromdate: string = ""
   todate: string = ""
   ptypes: any[] = []
-  pos_report: { summary: any[], transactions: any[], showtransaxns: any[] } = { summary: [], transactions: [], showtransaxns: [] }
+  splitpayment: {
+    isvalid: boolean;
+    remarks: string;
+    transactions: Transaction[];
+    addtrnsxn: () => void;
+    delete: (i: number) => void;
+    validate: () => void;
+  } = {
+      isvalid: true, remarks: "", transactions: [], addtrnsxn: () => {
+        if (this.selectedtransaxn && this.splitpayment.transactions.length < this.ptypes.length - 1) {
+          let tr = new Transaction(this.companyid, this.storeid, this.selectedtransaxn.OrderId)
+          tr.Amount = this.selectedtransaxn.ogAmount - this.selectedtransaxn.Amount - this.splitpayment.transactions.map((x: any) => x.Amount).reduce((a: any, c: any) => a + c, 0)
+          tr.StorePaymentTypeId = this.ptypes.filter(x => ![this.selectedtransaxn.StorePaymentTypeId, ...this.splitpayment.transactions.map(y => y.StorePaymentTypeId)].includes(x.Id))[0].Id
+          tr.StorePaymentTypeName = this.ptypes.filter(x => x.Id == tr.StorePaymentTypeId)[0].Name
+          tr.InvoiceNo = this.selectedtransaxn.ino
+          tr.TransDate = moment(this.selectedtransaxn.TransDate).format("YYYY-MM-DD")
+          tr.TransDateTime = this.selectedtransaxn.TransDateTime
+          this.splitpayment.transactions.push(tr)
+        }
+      },
+      delete: (i: number) => {
+        this.splitpayment.transactions.splice(i, 1)
+      },
+      validate: () => {
+        this.splitpayment.isvalid = true
+        let total = this.selectedtransaxn.Amount + this.splitpayment.transactions.map(x => x.Amount).reduce((a, c) => a + c, 0)
+        if (this.splitpayment.transactions.filter(x => x.Amount > 0).length > 0 && total > this.selectedtransaxn.ogAmount) {
+          this.splitpayment.isvalid = false
+          this.splitpayment.remarks = `total amount - ${total} - exceeds original amount ${this.selectedtransaxn.ogAmount} by ${total - this.selectedtransaxn.ogAmount}`
+        }
+        this.splitpayment.transactions.forEach(t => {
+          t.TransDateTime = moment(t.TransDate).format("YYYY-MM-DD") + moment(t.TransDateTime).format(" hh:mm:ss")
+        })
+      }
+    }
+  pos_report: { summary: any[], transactions: any[], showtransaxns: any[], total: { bills: number, amount: number } } = { summary: [], transactions: [], showtransaxns: [], total: { bills: 0, amount: 0 } }
 
   constructor(private auth: AuthService, private modalService: NgbModal) { }
 
@@ -72,9 +109,12 @@ export class TransactionListComponent implements OnInit {
     })
   }
   getTransactionList() {
+    if (this.storeid == 0) {
+      return
+    }
     this.auth.getstorepaymentsbytype(this.storeid, this.companyid, this.fromdate, this.todate).subscribe((data: any) => {
-      this.selectedtransaxn = null;
-      this.pos_report = { summary: data["pos_summary"], transactions: data["pos_transactions"], showtransaxns: data["pos_transactions"].filter((x: any) => x.StorePaymentTypeId == data["pos_summary"][0].StorePaymentTypeId) }
+      this.selectedtransaxn = new Transaction(this.companyid, this.storeid, 0);
+      this.pos_report = { summary: data["pos_summary"], total: { bills: data["pos_summary"].map((x: any) => x.BillCount).reduce((a: number, c: number) => a + c, 0), amount: data["pos_summary"].map((x: any) => x.Amount).reduce((a: number, c: number) => a + c, 0) }, transactions: data["pos_transactions"], showtransaxns: data["pos_transactions"].filter((x: any) => x.StorePaymentTypeId == data["pos_summary"][0].StorePaymentTypeId) }
     })
   }
   setDateRange() {
@@ -117,18 +157,48 @@ export class TransactionListComponent implements OnInit {
     this.pos_report.showtransaxns = this.pos_report.transactions.filter((x: any) => x.StorePaymentTypeId == this.pos_report.summary[i].StorePaymentTypeId)
   }
   selectedtransaxn: any
+  date: string = ''
+  time: string = ''
   editTransaction(i: number, event: MouseEvent) {
     this.selectedtransaxn = this.copyworef(this.pos_report.showtransaxns[i])
+    this.selectedtransaxn.StorePaymentTypeId = +this.selectedtransaxn.StorePaymentTypeId
+    this.selectedtransaxn.ogAmount = +this.selectedtransaxn.Amount
+    this.date = moment(this.selectedtransaxn.TransDate).format("YYYY-MM-DD")
     console.log(this.selectedtransaxn)
-    let popup = document.getElementById("ptype-pop") as HTMLElement
-    let srcEl = event.target as HTMLElement
-    popup.style.top = (event.pageY - (popup.clientHeight / 2)) + 'px'
-    popup.style.left = (20 + event.pageX + (srcEl.clientWidth - event.offsetX)) + 'px'
+    this.modalService.open(this.transactioneditmdl, {
+      centered: true,
+      size: 'lg',
+      windowClass: 'float-right',
+      backdropClass: 'z-index-1',
+    })
+    // let popup = document.getElementById("ptype-pop") as HTMLElement
+    // let srcEl = event.target as HTMLElement
+    // popup.style.top = (event.pageY - (popup.clientHeight / 2)) + 'px'
+    // popup.style.left = (20 + event.pageX + (srcEl.clientWidth - event.offsetX)) + 'px'
   }
   updatetransaction() {
+    if (this.selectedtransaxn.Amount <= 0) {
+      return
+    }
+    this.splitpayment.validate()
+    if (!this.splitpayment.isvalid) {
+      return
+    }
+    this.selectedtransaxn.TransDate = moment(this.date).format("YYYY-MM-DD")
+    this.selectedtransaxn.TransDateTime = moment(this.date).format("YYYY-MM-DD") + moment(this.selectedtransaxn.TransDateTime).format(" hh:mm:ss")
+    this.splitpayment.transactions = this.splitpayment.transactions.filter(x => x.Amount > 0)
+    // console.log(this.selectedtransaxn)
+    // return
     this.auth.savetransaction(this.selectedtransaxn).subscribe(data => {
-      // console.log(data)
-      this.getTransactionList();
+      if (this.splitpayment.transactions.length > 0) {
+        this.auth.ordertransaction(this.splitpayment.transactions).subscribe(data => {
+          this.getTransactionList();
+          this.modalService.dismissAll()
+        })
+      } else {
+        this.getTransactionList();
+        this.modalService.dismissAll()
+      }
     })
   }
   transaxnlsttxt: string = ""
@@ -148,7 +218,7 @@ export class TransactionListComponent implements OnInit {
     let arr: any[] = []
     splitData.slice(1).forEach(line => {
       let obj: any = {}
-      headers.forEach((hdr,i) => {
+      headers.forEach((hdr, i) => {
         obj[hdr] = line[i]
       })
       obj["Net Amount"] = +obj["Net Amount"]
