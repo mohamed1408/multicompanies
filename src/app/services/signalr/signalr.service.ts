422845d8d275ff4e5d6b1d8da1d676883d64a863
HyperTechs <hypertech.rj@gmail.com>
2024-03-11 11:31:16 +0530

    #addsignalr_MS_HY

--- src/app/services/signalr/signalr.service.ts
+++ src/app/services/signalr/signalr.service.ts
@@ -1,71 +1,74 @@
 import { Injectable } from '@angular/core';
-import * as signalR from '@microsoft/signalr'
+import * as signalR from '@microsoft/signalr';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SignalrService {
-  base_url1 = 'https://localhost:44383/'
-  base_url = 'https://biz1ps.azurewebsites.net/'
-  base_urlc = 'https://biz1posapi-rv7.conveyor.cloud/'
+  base_url1 = 'https://localhost:44383/';
+  base_url = 'https://biz1pos.azurewebsites.net/';
+  base_urlc = 'https://biz1posapi-rv7.conveyor.cloud/';
 
   hubconnection: signalR.HubConnection = new signalR.HubConnectionBuilder()
     .withUrl(this.base_url + 'uphub')
     .withAutomaticReconnect([0, 1000, 5000, 10000])
     .configureLogging(signalR.LogLevel.Information)
-    .build()
-  CompanyId: any
-  StoreId: any
-  hubRoom: any
-  isconnected: boolean = false
-  logInfo: any
-  shouldConnect: boolean = true
-  notificationobj = {}
-  orders = []
+    .build();
+  CompanyId: any;
+  StoreId: any;
+  hubRoom: any;
+  isconnected: boolean = false;
+  logInfo: any;
+  shouldConnect: boolean = true;
+  notificationobj = {};
+  orders = [];
   autoaccepttime!: number;
   foodpreptime!: number;
-  bot: any
+  bot: any;
 
   constructor() {
-    // this.hubconnection.on('DeliveryOrderUpdate', (fromstoreid, tostoreid, invoiceno) => {
-    //   if (invoiceno.includes("ENQ")) {
-    //     console.log('DeliveryOrderUpdate', fromstoreid, tostoreid, invoiceno)
-    //   }
-    // })
+    this.hubconnection.on(
+      'DeliveryOrderUpdate',
+      (fromstoreid, tostoreid, invoiceno) => {
+        if (invoiceno.includes('WO')) {
+          console.log('DeliveryOrderUpdate', fromstoreid, tostoreid, invoiceno);
+        }
+      }
+    );
 
     this.hubconnection.on('OrderUpdate', (orderid, storeid) => {
       // console.log('OrderUpdate', orderid, storeid)
-    })
+    });
 
     this.hubconnection.on('DeliveryOrderUpdate', (orderid, storeid) => {
       // console.log('OrderUpdate', orderid, storeid)
-    })
+    });
 
     this.hubconnection.on('NewOrder', (platfor, orderid) => {
       // console.log('NewOrder', platfor, orderid)
-    })
+    });
 
-    this.hubconnection.onreconnecting(err => {
-      console.log('<😱>', err)
-    })
+    this.hubconnection.onreconnecting((err) => {
+      console.log('<😱>', err);
+    });
 
-    this.hubconnection.onreconnected(connectionid => {
-      console.log(connectionid)
-    })
+    this.hubconnection.onreconnected((connectionid) => {
+      console.log(connectionid);
+    });
   }
 
   connect() {
     this.hubconnection
       .start()
       .then(() => {
-        console.log('Connection started! <😎>')
-        this.isconnected = true
+        console.log('Connection started! <😎>');
+        this.isconnected = true;
       })
-      .catch(err => {
+      .catch((err) => {
         setTimeout(() => {
-          this.connect()
-          console.log(err)
-        }, 2000)
-      })
+          this.connect();
+          console.log(err);
+        }, 2000);
+      });
   }
 }
